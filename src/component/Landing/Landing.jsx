3a9efa963afab15fa9ef9808f17bf15b09e903fd
Erodri6z <erikrodriguez2417@gmail.com>
2022-09-12 11:49:37 -0500

    add styling to about page

--- src/component/Landing/Landing.jsx
+++ src/component/Landing/Landing.jsx
@@ -5,7 +5,7 @@ const Landing = () => {
     <div className="landing-page">
       <div >
         <h1 id='landing-title'>Hi,<br /> I'm Erik,<br /> Software Engineer</h1>
-        <p id='landing-text'>Technology is always evolving, let's perfect it</p>
+        <p id='landing-text'>Technology is always evolving, let's perfect it!</p>
       </div>
     </div>
   )
