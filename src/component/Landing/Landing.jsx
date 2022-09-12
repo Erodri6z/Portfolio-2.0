f4d2a9c89937ea9c6f7c71f4b2550f27ce03931f
Erodri6z <erikrodriguez2417@gmail.com>
2022-09-12 11:28:33 -0500

    add styling to landing

--- src/component/Landing/Landing.jsx
+++ src/component/Landing/Landing.jsx
@@ -3,7 +3,10 @@ import '../../styles/landing.css'
 const Landing = () => {
   return(
     <div className="landing-page">
-      <h1>Hello this is the landing</h1>
+      <div >
+        <h1 id='landing-title'>Hi,<br /> I'm Erik,<br /> Software Engineer</h1>
+        <p id='landing-text'>Technology is always evolving, let's perfect it</p>
+      </div>
     </div>
   )
 }
