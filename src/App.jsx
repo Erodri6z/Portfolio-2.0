f0e94c93965ef0c77362d0b98968f47e38adf749
Erodri6z <erikrodriguez2417@gmail.com>
2022-09-11 20:07:44 -0500

    add an about page

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom'
 import './App.css';
 
-import Nav from './component/Nav/Nav';
+import Nav from './component/Nav/Nav'
+import About from './component/About/About';
 
 function App() {
   return (
     <main>
       <Nav className="nav" />
       <Routes>
+        {/* <Route path='/' element={<Landing />} /> */}
+        <Route path='/info' element={<About />} />
 
       </Routes>
     </main>
---
+++ src/component/About/About.jsx
@@ -0,0 +1,8 @@
+const About = () => {
+  return (
+    <h1>hello yes aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</h1>
+  )
+}
+
+
+export default About
