8385d1bb6fea6532433f24be8cdbc4c63598d338
Erodri6z <erikrodriguez2417@gmail.com>
2022-09-12 17:14:49 -0500

    add a skills page

--- src/component/Skill/Skill.jsx
+++ src/component/Skill/Skill.jsx
@@ -3,7 +3,59 @@ import '../../styles/skill.css'
 const Skill = () => {
   return(
     <div className="skill-page">
-      <h1>hello this is the skills </h1>
+      <div id='skill-container'>
+        <h1>Technical Skills</h1>
+        <div className="icons">
+          <div>
+            <img src="https://img.shields.io/badge/-HTML5-333333?style=flat&logo=HTML5" alt="html" id="html" />
+          </div>
+          <div>
+            <img src="https://img.shields.io/badge/-CSS-333333?style=flat&logo=CSS3&logoColor=1572B6" alt="css" id="css" />
+          </div>
+          <div>
+            <img src="https://img.shields.io/badge/-JavaScript-333333?style=flat&logo=Javascript&logoColor=)" alt="javascript" id="javascript"/>
+          </div>
+          <div>
+            <img src="https://img.shields.io/badge/-React-333333?logo=react" alt="react" id="react" />
+          </div>
+          <div>
+            <img src="https://img.shields.io/badge/-Python3-333333?style=flat&logo=python" alt="python" id="python" />
+          </div>
+          <div>
+            <img src="https://img.shields.io/badge/-Express-333333?style=flat&logo=express" alt="express" id="express" />
+          </div>
+          <div>
+            <img src="https://img.shields.io/badge/-mongoDB-333333?logo=mongodb" alt="mongodb" id="mongodb" />
+          </div>
+          <div>
+            <img src="https://img.shields.io/badge/-Github-333333?logo=github" alt="github" id="github-badge" />
+          </div> 
+          <div>
+            <img src="https://img.shields.io/badge/-Node.js-333333?logo=node.js" alt="nodejs" id="nodejs" />
+          </div>
+          <div>
+            <img src="https://img.shields.io/badge/-PostgreSQL-333333?logo=PostgreSQL" alt="postgreSQL" id="postgresSQL" />
+          </div>
+          <div>
+            <img src="https://img.shields.io/badge/-Django-333333?logo=django" alt="django" id="django" />
+          </div>
+          <div>
+            <img src="https://img.shields.io/badge/-Docker-333333?logo=docker" alt="docker" id="docker" />
+          </div>
+          <div>
+            <img src="https://img.shields.io/badge/-Netlify-333333?logo=netlify" alt="netlify" id="netlify" />
+          </div>
+          <div>
+            <img src="https://img.shields.io/badge/-Heroku-333333?logo=heroku" alt="heroku" id="heroku" />
+          </div>
+          <div>
+            <img src="https://img.shields.io/badge/-Bootstap-333333?logo=bootstrap" alt="bootstrap" id="bootstrap" />
+          </div>
+          <div>
+            <img src="https://img.shields.io/badge/-Git-333333?logo=git" alt="git" id="git"/>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
