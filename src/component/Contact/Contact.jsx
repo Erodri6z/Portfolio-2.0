0d5b6eeddf00badb6d3e1a69a44f4c65e69ecfb7
Erodri6z <erikrodriguez2417@gmail.com>
2022-09-13 21:13:53 -0500

    add icons and contact info to contact

--- src/component/Contact/Contact.jsx
+++ src/component/Contact/Contact.jsx
@@ -1,9 +1,38 @@
 import '../../styles/contact.css'
 
 const Contact = () => {
+  const contactInfo = [{
+    name: 'Github',
+    url: 'https://github.com/Erodri6z',
+    icon: <i className="bi bi-github"></i> ,
+  },{
+    name: 'Email',
+    url: 'mailto:erikrodriguez2417@gmail.com',
+    icon: <i className="bi bi-envelope"></i>,
+  },{
+    name: 'LinkedIn',
+    url: 'https://www.linkedin.com/in/erik-r-b02085200/',
+    icon: <i className="bi bi-linkedin"></i>,
+  },{
+    name: 'Resume',
+    url: 'https://docs.google.com/document/d/1GNk5k53wbv4a2SR02lePJ9MVVd2QykYr8UFt54IqI0M/edit?usp=sharing',
+    icon: <i className="bi bi-file-earmark-person-fill"></i>,
+  }]
   return(
     <div className="contact-page">
-      <h1>Hello this is the contact</h1>
+      <h1>Contact Me</h1>
+      <div className='contact-links'>
+        <>
+        {contactInfo.map(icon => 
+        <div className="contact-icons" key={icon.name}>
+          <a href={icon.url}>
+            <h1>{icon.icon}</h1> 
+            <h3>{icon.name}</h3>
+          </a>
+        </div>
+          )}
+        </>
+      </div>
     </div>
   )
 }
--- src/component/Projects/Projects.jsx
+++ src/component/Projects/Projects.jsx
@@ -6,31 +6,29 @@ import strangerDrinks from '../../assets/strangerdrinks.png'
 
 const Projects = () => {
   const projectData = [{
-
-        title: 'Tic-Tac-Toe',
-        deployment: 'https://ttt-erodri6z.netlify.app/',
-        image: ticTacToe,
-        github:'https://github.com/Erodri6z/TicTacToe',
-        description: 'Fully coded in javascript, CSS, and HTML. Built a minimal Tic-Tac-Toe browser game. Learned the basics of app logic as well as app deployment'
-
+      title: 'Tic-Tac-Toe',
+      deployment: 'https://ttt-erodri6z.netlify.app/',
+      image: ticTacToe,
+      github:'https://github.com/Erodri6z/TicTacToe',
+      description: 'Fully coded in javascript, CSS, and HTML. Built a minimal Tic-Tac-Toe browser game. Learned the basics of app logic as well as app deployment'
     },{
-        title: "Skill-Tree",
-        deployment: 'https://erodri6z-skill-tree.herokuapp.com/',
-        image: skillTree,
-        github: 'https://github.com/Erodri6z/Skill-Tree',
-        description: 'This was an app meant to gather your skills and share them with others. Used full MEN stack, HTML, CSS, JavaScript, GoogleAuth, and EJS. This app helped me learn about GoogleAuth, ERDs, and Heroku deployment'
+      title: "Skill-Tree",
+      deployment: 'https://erodri6z-skill-tree.herokuapp.com/',
+      image: skillTree,
+      github: 'https://github.com/Erodri6z/Skill-Tree',
+      description: 'This was an app meant to gather your skills and share them with others. Used full MEN stack, HTML, CSS, JavaScript, GoogleAuth, and EJS. This app helped me learn about GoogleAuth, ERDs, and Heroku deployment'
     }, {
-        title: "Campfire",
-        deployment: 'https://campfire-erodri6z.herokuapp.com/',
-        image: campFire,
-        github: 'https://github.com/Erodri6z/CampFire',
-        description: 'An app that can be used to share and find story ideas. Used Django, Python3, Docker, HTML and CSS. This app helped me learn Python and PostgreSQL'
+      title: "Campfire",
+      deployment: 'https://campfire-erodri6z.herokuapp.com/',
+      image: campFire,
+      github: 'https://github.com/Erodri6z/CampFire',
+      description: 'An app that can be used to share and find story ideas. Used Django, Python3, Docker, HTML and CSS. This app helped me learn Python and PostgreSQL'
     }, {
-        title: "Stranger Drinks",
-        deployment: 'https://stranger-drinks.netlify.app/',
-        image: strangerDrinks,
-        github: 'https://github.com/psison92/stranger-drinks-front-end/issues',
-        description: 'It’s a “Stranger Things”  themed app to be able to share drink ideas and recipies. An MERN stack app built in collaboration with 3 other teammates. Used full MERN stack, HTML, CSS, and Javascript. This app helped me learn React.js, JWT authentication, and Github group workflow'
+      title: "Stranger Drinks",
+      deployment: 'https://stranger-drinks.netlify.app/',
+      image: strangerDrinks,
+      github: 'https://github.com/psison92/stranger-drinks-front-end/issues',
+      description: 'It’s a “Stranger Things”  themed app to be able to share drink ideas and recipies. An MERN stack app built in collaboration with 3 other teammates. Used full MERN stack, HTML, CSS, and Javascript. This app helped me learn React.js, JWT authentication, and Github group workflow'
     }
   ]
   return(
@@ -53,7 +51,6 @@ const Projects = () => {
           )}
         </>
       </div>
-      
     </div>
   )
 }
