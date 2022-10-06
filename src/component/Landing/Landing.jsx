9a1e657aa49c19f8c30e1978b20642e276721734
Erodri6z <erikrodriguez2417@gmail.com>
2022-10-05 20:01:41 -0500

    fix bug on contact page

--- src/component/Landing/Landing.jsx
+++ src/component/Landing/Landing.jsx
@@ -5,7 +5,7 @@ const Landing = () => {
   return(
     <div className="landing-page">
       <div >
-        <h1 id='landing-title'>Hi,<br /> I'm Erik,<br /> Software Engineer</h1>
+        <h1 id='landing-title'>Hi,<br /> I'm Erik Rodriguez,<br /> Software Engineer</h1>
         <p id='landing-text'>Technology is always evolving, let's perfect it!</p>
       </div>
     </div>
