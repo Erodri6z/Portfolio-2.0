5c0fa647ddc260c3624b173ac968d49e88fd908c
Erodri6z <erikrodriguez2417@gmail.com>
2023-08-11 11:58:25 -0500

    add better badges to skills

--- src/component/Skill/Skill.jsx
+++ src/component/Skill/Skill.jsx
@@ -7,58 +7,58 @@ const Skill = () => {
         <h1>Technical Skills</h1>
         <div className="icons">
           <div>
-            <img src="https://img.shields.io/badge/-HTML5-000000?style=flat&logo=HTML5" alt="html" id="html" />
+            <img src="https://img.shields.io/badge/-HTML5-000000?style=for-the-badge&logo=HTML5" alt="html" id="html" />
           </div>
           <div>
-            <img src="https://img.shields.io/badge/-CSS-000000?style=flat&logo=CSS3&logoColor=1572B6" alt="css" id="css" />
+            <img src="https://img.shields.io/badge/-CSS-000000?style=for-the-badge&logo=CSS3&logoColor=1572B6" alt="css" id="css" />
           </div>
           <div>
-            <img src="https://img.shields.io/badge/-JavaScript-000000?style=flat&logo=Javascript&logoColor=)" alt="javascript" id="javascript"/>
+            <img src="https://img.shields.io/badge/-JavaScript-000000?style=for-the-badge&logo=Javascript&logoColor=)" alt="javascript" id="javascript"/>
           </div>
           <div>
-            <img src="https://img.shields.io/badge/-React.js-000000?logo=react" alt="react" id="react" />
+            <img src="https://img.shields.io/badge/-React.js-000000?style=for-the-badge&logo=react" alt="react" id="react" />
           </div>
           <div>
-            <img src="https://img.shields.io/badge/-Python3-000000?style=flat&logo=python" alt="python" id="python" />
+            <img src="https://img.shields.io/badge/-Python3-000000?style=for-the-badge&logo=python" alt="python" id="python" />
           </div>
           <div>
-            <img src="https://img.shields.io/badge/-Express.js-000000?style=flat&logo=express" alt="express" id="express" />
+            <img src="https://img.shields.io/badge/-Express.js-000000?style=for-the-badge&logo=express" alt="express" id="express" />
           </div>
           <div>
-            <img src="https://img.shields.io/badge/-MongoDB-000000?logo=mongodb" alt="mongodb" id="mongodb" />
+            <img src="https://img.shields.io/badge/-MongoDB-000000?style=for-the-badge&logo=mongodb" alt="mongodb" id="mongodb" />
           </div>
           <div>
-            <img src="https://img.shields.io/badge/-Github-000000?logo=github" alt="github" id="github-badge" />
+            <img src="https://img.shields.io/badge/-Github-000000?style=for-the-badge&logo=github" alt="github" id="github-badge" />
           </div> 
           <div>
-            <img src="https://img.shields.io/badge/-Node.js-000000?logo=node.js" alt="nodejs" id="nodejs" />
+            <img src="https://img.shields.io/badge/-Node.js-000000?style=for-the-badge&logo=node.js" alt="nodejs" id="nodejs" />
           </div>
           <div>
-            <img src="https://img.shields.io/badge/-PostgreSQL-000000?logo=PostgreSQL" alt="postgreSQL" id="postgresSQL" />
+            <img src="https://img.shields.io/badge/-PostgreSQL-000000?style=for-the-badge&logo=PostgreSQL" alt="postgreSQL" id="postgresSQL" />
           </div>
           <div>
-            <img src="https://img.shields.io/badge/-Django-000000?logo=django&logoColor=1572B6" alt="django" id="django" />
+            <img src="https://img.shields.io/badge/-Django-000000?style=for-the-badge&logo=django&logoColor=1572B6" alt="django" id="django" />
           </div>
           <div>
-            <img src="https://img.shields.io/badge/-Docker-000000?logo=docker" alt="docker" id="docker" />
+            <img src="https://img.shields.io/badge/-Docker-000000?style=for-the-badge&logo=docker" alt="docker" id="docker" />
           </div>
           <div>
-            <img src="https://img.shields.io/badge/-Netlify-000000?logo=netlify" alt="netlify" id="netlify" />
+            <img src="https://img.shields.io/badge/-Netlify-000000?style=for-the-badge&logo=netlify" alt="netlify" id="netlify" />
           </div>
           <div>
-            <img src="https://img.shields.io/badge/-Heroku-000000?logo=heroku&logoColor=6F2EE0" alt="heroku" id="heroku" />
+            <img src="https://img.shields.io/badge/-Heroku-000000?style=for-the-badge&logo=heroku&logoColor=6F2EE0" alt="heroku" id="heroku" />
           </div>
           <div>
-            <img src="https://img.shields.io/badge/-Bootstap-000000?logo=bootstrap" alt="bootstrap" id="bootstrap" />
+            <img src="https://img.shields.io/badge/-Bootstap-000000?style=for-the-badge&logo=bootstrap" alt="bootstrap" id="bootstrap" />
           </div>
           <div>
-            <img src="https://img.shields.io/badge/-Git-000000?logo=git" alt="git" id="git"/>
+            <img src="https://img.shields.io/badge/-Git-000000?style=for-the-badge&logo=git" alt="git" id="git"/>
           </div>
           <div>
-            <img src="https://img.shields.io/badge/-Next.js-000000?logo=next.js" alt="next" id='next' />
+            <img src="https://img.shields.io/badge/-Next.js-000000?style=for-the-badge&logo=next.js" alt="next" id='next' />
           </div>
           <div>
-            <img src="https://img.shields.io/badge/-Sass-000000?logo=Sass" alt="sass" id="sass" />
+            <img src="https://img.shields.io/badge/-Sass-000000?style=for-the-badge&logo=Sass" alt="sass" id="sass" />
           </div>
         </div>
       </div>
