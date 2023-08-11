286c6ece7aacd17c1882a2fe5bdb74231bb419eb
Erodri6z <erikrodriguez2417@gmail.com>
2023-08-11 16:06:49 -0500

    add a function that will change the title in landing

--- src/component/Landing/Landing.jsx
+++ src/component/Landing/Landing.jsx
@@ -2,10 +2,21 @@ import '../../styles/landing.scss'
 
 const Landing = () => {
   
+
+  const titles = ["Software Engineer", "Front-End Engineer", "Back-end Engineer", "Full-Stack Developer", "Web Developer"]
+
+
+  const chooseATitle = () => {
+    let x = Math.floor((Math.random() * 4) + 1)
+    return x
+  }
+
+  const titleChosen = titles[chooseATitle()]
+
   return(
     <div className="landing-page">
-      <div >
-        <h1 id='landing-title'>Hi,<br /> I'm Erik Rodriguez,<br /> Software Engineer</h1>
+      <div>
+        <h1 id='landing-title'>Hi,<br /> I'm Erik Rodriguez,<br /> {titleChosen}</h1>
         <p id='landing-text'>Technology is always evolving, let's perfect it!</p>
       </div>
     </div>
