36c237ca6f31988b658b93d70f84e9d0fa99e3e2
Erodri6z <erikrodriguez2417@gmail.com>
2023-08-11 16:10:18 -0500

    reformat the title function in landing

--- src/component/Landing/Landing.jsx
+++ src/component/Landing/Landing.jsx
@@ -2,21 +2,18 @@ import '../../styles/landing.scss'
 
 const Landing = () => {
   
-
-  const titles = ["Software Engineer", "Front-End Engineer", "Back-end Engineer", "Full-Stack Developer", "Web Developer"]
-
-
   const chooseATitle = () => {
+    const titles = ["Software Engineer", "Front-End Engineer", "Back-end Engineer", "Full-Stack Developer", "Web Developer"]
     let x = Math.floor((Math.random() * 4) + 1)
-    return x
+    let titleChosen = titles[x]
+    return titleChosen
   }
 
-  const titleChosen = titles[chooseATitle()]
 
   return(
     <div className="landing-page">
       <div>
-        <h1 id='landing-title'>Hi,<br /> I'm Erik Rodriguez,<br /> {titleChosen}</h1>
+        <h1 id='landing-title'>Hi,<br /> I'm Erik Rodriguez,<br /> {chooseATitle()}</h1>
         <p id='landing-text'>Technology is always evolving, let's perfect it!</p>
       </div>
     </div>
