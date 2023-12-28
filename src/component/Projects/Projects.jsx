cf56a0e1bb32dc0cafebc2a49c4edb197afb5a9e
Erodri6z <erikrodriguez2417@gmail.com>
2023-12-28 13:56:43 -0600

    add a new project to the projects page

--- src/component/Projects/Projects.jsx
+++ src/component/Projects/Projects.jsx
@@ -6,6 +6,7 @@ import strangerDrinks from '../../assets/strangerdrinks.png'
 import pocketGeeks from '../../assets/PocketGeeks.png'
 import unlimitedMovingAndStorage from '../../assets/unlimitedMoving.png'
 import gloomySpirits from './../../assets/gloomyspirits.png'
+import saleCounter from './../../assets/Sale-counter.png'
 
 const Projects = () => {
   const projectData = [{
@@ -50,6 +51,11 @@ const Projects = () => {
       image: gloomySpirits,
       github: "https://github.com/Erodri6z/Drink-Assist",
       description: "This Gloomy Spirits app is your ultimate companion for crafting the perfect cocktails. Powered by TypeScript and React, it allows you to effortlessly search for cocktail recipes by name or ingredients. With detailed instructions and a user-friendly design, it's a go-to resource for both novice mixologists and seasoned cocktail enthusiasts, ensuring that you can create impressive drinks for any occasion."
+    }, {
+      title:"Sale Counter",
+      deployment:"https://sale-counter.netlify.app",
+      image: saleCounter,
+      description: "The Mobile Sales Tracker App, built with React.js, is specifically designed to assist mobile representatives in accurately tracking their sales activities. The application not only enables representatives to input and manage sales data efficiently but also provides visual graphs that accurately depict the number of sales completed in each respective category"
     }
   ]
   return(
